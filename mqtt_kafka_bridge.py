afe21cf99e4034aa79fac2837463cdbe1be3816d
pilac96 <efea2010@gmail.com>
2023-06-02 15:11:43 +0200

    Added decoding to the message

--- mqtt_kafka_bridge.py
+++ mqtt_kafka_bridge.py
@@ -14,7 +14,7 @@ kafka_producer = kafka_topic.get_sync_producer()
 
 
 def on_message(client, userdata, message):
-    msg_payload = str(message.payload)
+    msg_payload = str(message.payload.decode('utf-8'))
     print("Received MQTT message: ", msg_payload)
     kafka_producer.produce(msg_payload.encode('ascii'))
     print("KAFKA: Just published {} to topic {}".format(msg_payload, topic))
